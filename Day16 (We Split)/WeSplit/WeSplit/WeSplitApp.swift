a0ae1f0d054ae8b32416a873af2b3ad7bfccf20b
pedroacevedo <pedro.acevedo@solbeautyandcare.com>
2024-02-19 15:18:37 -0800

    Day 16 We Split

---
+++ Day16 (We Split)/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pedro Acevedo on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
