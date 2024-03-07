00079cbdd3a965544a9c69bbcfcee2ae4dd150ef
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-03-07 11:18:16 -0800

    Day 20, 21, 22 Guess the flag

---
+++ Day20 (Guess the flag)/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Pedro Acevedo on 05/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var score = 0
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.secondary)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                Text("Score: " + String(score))
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is " + String(score))
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            if (score >= 8) {
+                scoreTitle = "Correct, game over"
+                score = 0
+            } else {
+                scoreTitle = "Correct"
+                score += 1
+            }
+        } else {
+            scoreTitle = "Wrong thats the flag of " + countries[number]
+        }
+
+        showingScore = true
+    }
+}
+
+#Preview {
+    ContentView()
+}
