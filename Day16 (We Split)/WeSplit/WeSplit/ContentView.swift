2eb2c53f8fe35424f7ca7f4b21dc293cc0f6dfc4
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-03-13 13:23:28 -0700

    Day 24 challenge

--- Day16 (We Split)/WeSplit/WeSplit/ContentView.swift
+++ Day16 (We Split)/WeSplit/WeSplit/ContentView.swift
@@ -57,6 +57,7 @@ struct ContentView: View {
                         Text(totalPerPerson, format: .currency(
                             code: Locale.current.currency?.identifier ?? "USD")
                         )
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
                     }
                 }
             }
