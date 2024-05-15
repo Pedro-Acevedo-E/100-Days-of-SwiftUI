bd8e15b8f64296fbb681664b2a288582008f115b
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-05-15 14:06:10 -0700

    Day 39, initial project creation

---
+++ Day40 (Moonshot)/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Pedro Acevedo on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
