d5431b715203249af1036ed09f44726eb878fd3c
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-05-29 15:01:39 -0700

    Day 51, Cupcake Corner initial sample project

--- Day49 (CupcakeCorner)/CupcakeCorner/CupcakeCorner/Views/AdressView.swift
+++ Day49 (CupcakeCorner)/CupcakeCorner/CupcakeCorner/Views/AdressView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct AddressView: View {
-    var order: Order
+    @Bindable var order: Order
 
     var body: some View {
-        Text("Add sprinkles?: ")
-        Text(String(order.addSprinkles))
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
