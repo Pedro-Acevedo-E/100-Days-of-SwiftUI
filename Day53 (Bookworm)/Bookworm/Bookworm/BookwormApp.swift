8e94ce6bb8a655f020a090ba253dea983e19229b
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-06-04 14:12:27 -0700

    Day 53 Bookworm project creation

---
+++ Day53 (Bookworm)/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Pedro Acevedo on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
