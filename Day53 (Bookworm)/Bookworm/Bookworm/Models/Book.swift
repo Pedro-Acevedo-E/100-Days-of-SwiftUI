a608664a10043f049c07372053e163601fe6e7df
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-06-04 15:37:36 -0700

    Day 54-56 Bookworm project completion

---
+++ Day53 (Bookworm)/Bookworm/Bookworm/Models/Book.swift
@@ -0,0 +1,28 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by Pedro Acevedo on 04/06/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: String
+    var review: String
+    var rating: Int
+    var date: Date? = Date.now //Provide default value to properties you add later
+    
+    init(title: String, author: String, genre: String, review: String, rating: Int, date: Date) {
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.review = review
+        self.rating = rating
+        self.date = date
+    }
+}
