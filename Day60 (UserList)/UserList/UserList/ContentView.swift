efd2256a6f2a0a5ea04282603235a3770fdb065f
Pedro Acevedo <acevedoensisopedrogabriel@gmail.com>
2024-06-10 14:46:46 -0700

    Day 61 Challenge

--- Day60 (UserList)/UserList/UserList/ContentView.swift
+++ Day60 (UserList)/UserList/UserList/ContentView.swift
@@ -6,29 +6,50 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
-    @State private var users = [User]()
+    @Environment(\.modelContext) var modelContext
+    @Query var users: [User]
+    @State private var loading = false
     
     var body: some View {
         NavigationStack {
-            List(users) { user in
-                NavigationLink(value: user) {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(user.name)
-                                .font(.headline)
-                            Text(user.email)
+            if (loading) {
+                ProgressView()
+            } else {
+                List(users) { user in
+                    NavigationLink(value: user) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(user.name)
+                                    .font(.headline)
+                                Text(user.email)
+                            }
+                            Spacer()
+                            Text(user.isActive ? "Active" : "Inactive")
+                                .foregroundStyle(user.isActive ? .green : .red)
                         }
-                        Spacer()
-                        Text(user.isActive ? "Active" : "Inactive")
-                            .foregroundStyle(user.isActive ? .green : .red)
                     }
                 }
-            }
-            .navigationTitle("User List")
-            .navigationDestination(for: User.self) { selection in
-                DetailsView(user: selection)
+                .navigationTitle("User List")
+                .navigationDestination(for: User.self) { selection in
+                    DetailsView(user: selection)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Delete", systemImage: "trash") {
+                            deleteUsers()
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Delete", systemImage: "gobackward") {
+                            Task {
+                                await loadUsers()
+                            }
+                        }
+                    }
+                }
             }
         }
         .task {
@@ -37,12 +58,26 @@ struct ContentView: View {
     }
     
     func loadUsers() async {
+        loading = true
         do {
             if(users.isEmpty) {
                 let response = try await URLSession.shared.data(from: URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!)
                 let decoded = try JSONDecoder().decode([User].self, from: response.0)
-                users = decoded
+                try modelContext.transaction {
+                  for user in decoded {
+                      modelContext.insert(user)
+                  }
+                }
             }
+            loading = false
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func deleteUsers() {
+        do {
+            try modelContext.delete(model: User.self)
         } catch {
             print("Error: \(error.localizedDescription)")
         }
--- Day60 (UserList)/UserList/UserList/UserListApp.swift
+++ Day60 (UserList)/UserList/UserList/UserListApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct UserListApp: App {
@@ -13,5 +14,6 @@ struct UserListApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
